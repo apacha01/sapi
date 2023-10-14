324f287e803b69b28244cc4c9d03ad8dc9c4f96a
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-10-14 11:12:17 -0300

    Minor changes.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { router as v1PetsRouter } from './v1/routes/pets-routes.js';
-import error from './controllers/error-controller.js';
+import errorsController from './controllers/error-controller.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // v1 Routes
 app.use('/api/v1/pets', v1PetsRouter);
-app.use('*', error.notFound);
+app.use('*', errorsController.notFound);
 
 app.listen(PORT, () => {
 	console.log(`API is listening on port ${PORT}`);

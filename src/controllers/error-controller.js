2a7366a8bbd1f951d7678c77a603b113fe6e0197
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-10-15 12:44:16 -0300

    Fix page not found response.

--- src/controllers/error-controller.js
+++ src/controllers/error-controller.js
@@ -2,7 +2,7 @@ import ResponseNotFound from '../lib/response/ResponseNotFound.js';
 
 const notFound = (req, res) => {
 	const url = req.originalUrl;
-	res.status(404).json(new ResponseNotFound(`Page with path '${url}' not found.`));
+	res.status(404).json(new ResponseNotFound({}, `Page with path '${url}' not found.`));
 };
 
 export { notFound };

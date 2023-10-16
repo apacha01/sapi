1f3c695e5d1dbba3e38686f4aff5ef50096caf28
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-10-16 10:44:36 -0300

    Remove unused file.

--- src/lib/response/ResponseOk.js
+++
@@ -1,9 +0,0 @@
-import Response from './Response.js';
-
-class ResponseOk extends Response {
-	constructor(content, extra, documentationURL) {
-		super(Response.HTTP_STATUS.OK.code, Response.HTTP_STATUS.OK.msg, content, extra, documentationURL);
-	}
-}
-
-export default ResponseOk;

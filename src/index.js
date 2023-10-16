57e50d0c8614c691c12731e9f004946cb080ad13
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-10-16 00:12:44 -0300

    Replace express-validator for ajv. Installed logger.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { router as v1PetsRouter } from './v1/routes/pets-routes.js';
 import errorsController from './controllers/error-controller.js';
+import pino from 'pino-http';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
+app.use(pino());
 
 // v1 Routes
 app.use('/api/v1/pets', v1PetsRouter);

bbbe73a6fafcd1cf7504767bd5c36a2f268bf1a9
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-10-21 17:39:31 -0300

    Added encryption and verification functions for passwords in db.

---
+++ src/lib/utils/encryption.js
@@ -0,0 +1,27 @@
+import crypto from 'node:crypto';
+
+const hash = (password) => {
+	return new Promise((resolve, reject) => {
+		const salt = crypto.randomBytes(16);
+		crypto.scrypt(password, salt, 32, (err, dk) => {
+			if (err) reject(err);
+			else resolve(Buffer.concat([salt, dk]).toString('hex'));
+		});
+	});
+};
+
+const verify = (password, hash) => {
+	return new Promise((resolve, reject) => {
+		const hashBuffer = Buffer.from(hash, 'hex');
+		const key = hashBuffer.subarray(16, hashBuffer.length);
+		const salt = hashBuffer.subarray(0, 16);
+
+		crypto.scrypt(password, salt, 32, (err, dk) => {
+			if (err) reject(err);
+			resolve(crypto.timingSafeEqual(key, dk));
+		});
+	});
+};
+
+export default { hash, verify };
+export { hash, verify };

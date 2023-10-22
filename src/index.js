ae64ffbfbe9a02c2d3b9515f220464f692d8fe37
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-10-22 11:02:33 -0300

    Refactor v1 routes into it's own router and add and enpoint to get the routes.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import express from 'express';
-import { router as v1PetsRouter } from './v1/routes/pets-routes.js';
-import { router as v1TokensRouter } from './v1/routes/tokens-routes.js';
-import { router as v1LoginRouter } from './v1/routes/login-routes.js';
-import { router as v1UsersRouter } from './v1/routes/users-routes.js';
 import { errorHandler, logger, pageNotFound } from './middlewares/error-handler.js';
 import dotenv from 'dotenv';
 import pino from 'pino-http';
+import { router as v1Router } from './v1/v1-routes.js';
 
 dotenv.config();
 
@@ -17,10 +14,7 @@ app.use(pino());
 app.use(express.json());
 
 // v1 Routes
-app.use('/api/v1/pets', v1PetsRouter);
-app.use('/api/v1/tokens', v1TokensRouter);
-app.use('/api/v1/login', v1LoginRouter);
-app.use('/api/v1/users', v1UsersRouter);
+app.use('/api/v1', v1Router);
 
 app.use(pageNotFound);
 app.use(logger);
---
+++ src/v1/v1-routes.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { router as loginRouter } from './routes/login-routes.js';
+import { router as petsRouter } from './routes/pets-routes.js';
+import { router as tokensRouter } from './routes/tokens-routes.js';
+import { router as usersRouter } from './routes/users-routes.js';
+
+const router = express.Router();
+
+router
+	.get('/', (req, res) => {
+		res.send(
+			{
+				login_url: 'https://DEPENDS_ON_HOSTING/api/v1/login',
+				pets_url: 'https://DEPENDS_ON_HOSTING/api/v1/pets',
+				tokens_url: 'https://DEPENDS_ON_HOSTING/api/v1/tokens',
+				users_url: 'https://DEPENDS_ON_HOSTING/api/v1/users'
+			}
+		);
+	})
+	.use('/login', loginRouter)
+	.use('/pets', petsRouter)
+	.use('/tokens', tokensRouter)
+	.use('/users', usersRouter);
+
+export { router };

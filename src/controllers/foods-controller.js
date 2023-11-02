0b578b125972044d62f651c046776857dac98e11
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-11-02 11:07:46 -0300

    Added logging to food controller.

--- src/controllers/foods-controller.js
+++ src/controllers/foods-controller.js
@@ -1,13 +1,20 @@
 import Response from '../lib/response/Response.js';
 import HTTP_STATUS from '../lib/constants/http.js';
 import foodsService from '../services/foods-service.js';
+import logger from '../lib/utils/logger.js';
+
+const flogger = logger.child({ model: 'Food', layer: 'Controller' });
 
 const getAllFoods = (req, res, next) => {
+
+	flogger.info('Calling service to get all foods');
 	foodsService.getAllFoods()
 		.then(result => {
+			flogger.info('Sending response with all foods');
 			res.status(HTTP_STATUS.OK.code).json(new Response(HTTP_STATUS.OK.code, HTTP_STATUS.OK.msg, result));
 		})
-		.catch((err) => {
+		.catch(err => {
+			flogger.error('Failed to get foods from service');
 			next(err);
 		});
 };
@@ -15,11 +22,14 @@ const getAllFoods = (req, res, next) => {
 const getFoodByName = (req, res, next) => {
 	const { foodName: name } = req.params;
 
+	flogger.info(`Calling service to get food with name '${name}'`);
 	foodsService.getFoodByName(name)
 		.then(result => {
+			flogger.info(`Sending response with food '${name}'`);
 			res.status(HTTP_STATUS.OK.code).json(new Response(HTTP_STATUS.OK.code, HTTP_STATUS.OK.msg, result));
 		})
-		.catch((err) => {
+		.catch(err => {
+			flogger.error(`Failed to get food with name '${name}' from service`);
 			next(err);
 		});
 };
@@ -27,30 +37,39 @@ const getFoodByName = (req, res, next) => {
 const createFood = (req, res, next) => {
 	const { food } = req.body;
 
+	flogger.info('Calling service to create food');
 	foodsService.createFood(food).then(result => {
+		flogger.info('Sending response with food created');
 		res.status(HTTP_STATUS.CREATED.code).json(new Response(HTTP_STATUS.CREATED.code, HTTP_STATUS.CREATED.msg, result, 'Food created.'));
 	}).catch(err => {
+		flogger.error('Service failed to create food');
 		next(err);
 	});
 };
 
 const updateFood = (req, res, next) => {
-	const { foodName } = req.params;
+	const { foodName: name } = req.params;
 	const { food } = req.body;
 
-	foodsService.updateFood(foodName, food).then(result => {
+	flogger.info(`Calling service to update food with name '${name}'`);
+	foodsService.updateFood(name, food).then(result => {
+		flogger.info('Sending response with food updated');
 		res.status(HTTP_STATUS.OK.code).json(new Response(HTTP_STATUS.OK.code, HTTP_STATUS.OK.msg, result, 'Food updated.'));
 	}).catch(err => {
+		flogger.info(`Failed to update food with name '${name}'`);
 		next(err);
 	});
 };
 
 const deleteFoodByName = (req, res, next) => {
-	const { foodName } = req.params;
+	const { foodName: name } = req.params;
 
-	foodsService.deleteFoodByName(foodName).then(result => {
+	flogger.info(`Calling service to delete food with name '${name}'`);
+	foodsService.deleteFoodByName(name).then(result => {
+		flogger.info('Sending response with food deleted');
 		res.status(HTTP_STATUS.OK.code).json(new Response(HTTP_STATUS.OK.code, HTTP_STATUS.OK.msg, result, 'Food deleted.'));
 	}).catch(err => {
+		flogger.info(`Failed to delete food with name '${name}'`);
 		next(err);
 	});
 };

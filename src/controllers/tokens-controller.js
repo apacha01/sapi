2ad5cc916eedf814d0bb2ff1b8cb3cb304261475
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-11-02 16:59:48 -0300

    Added logging to tokens controller.

--- src/controllers/tokens-controller.js
+++ src/controllers/tokens-controller.js
@@ -1,13 +1,19 @@
 import tokensService from '../services/tokens-service.js';
 import Response from '../lib/response/Response.js';
 import HTTP_STATUS from '../lib/constants/http.js';
+import logger from '../lib/utils/logger.js';
+
+const tlogger = logger.child({ model: 'Token', layer: 'Controller' });
 
 const getAllTokens = (req, res, next) => {
+	tlogger.info('Calling service to get all tokens');
 	tokensService.getAllTokens()
 		.then(result => {
+			tlogger.info('Sending response with all tokens');
 			res.status(HTTP_STATUS.OK.code).json(new Response(HTTP_STATUS.OK.code, HTTP_STATUS.OK.msg, result));
 		})
-		.catch((err) => {
+		.catch(err => {
+			tlogger.error('Failed to get tokens from service');
 			next(err);
 		});
 };
@@ -15,9 +21,12 @@ const getAllTokens = (req, res, next) => {
 const getTokenByName = (req, res, next) => {
 	const name = req.params.tokenName;
 
+	tlogger.info(`Calling service to get token with name '${name}'`);
 	tokensService.getTokenByName(name).then(result => {
+		tlogger.info(`Sending response with token '${name}'`);
 		res.status(HTTP_STATUS.OK.code).json(new Response(HTTP_STATUS.OK.code, HTTP_STATUS.OK.msg, result));
 	}).catch(err => {
+		tlogger.error(`Failed to get token with name '${name}' from service`);
 		next(err);
 	});
 };
@@ -25,9 +34,12 @@ const getTokenByName = (req, res, next) => {
 const createToken = (req, res, next) => {
 	const token = req.body.token;
 
+	tlogger.info('Calling service to create token');
 	tokensService.createToken(token).then(result => {
+		tlogger.info('Sending response with token created');
 		res.status(HTTP_STATUS.CREATED.code).json(new Response(HTTP_STATUS.CREATED.code, HTTP_STATUS.CREATED.msg, result, 'Token created.'));
 	}).catch(err => {
+		tlogger.error('Service failed to create token');
 		next(err);
 	});
 };
@@ -36,9 +48,12 @@ const updateTokenByName = (req, res, next) => {
 	const name = req.params.tokenName;
 	const token = req.body.token;
 
+	tlogger.info(`Calling service to update token with name '${name}'`);
 	tokensService.updateTokenByName(name, token).then(result => {
+		tlogger.info('Sending response with token updated');
 		res.status(HTTP_STATUS.OK.code).json(new Response(HTTP_STATUS.OK.code, HTTP_STATUS.OK.msg, result, 'Token updated.'));
 	}).catch(err => {
+		tlogger.info(`Failed to update token with name '${name}'`);
 		next(err);
 	});
 };
@@ -46,9 +61,12 @@ const updateTokenByName = (req, res, next) => {
 const deletePetByName = (req, res, next) => {
 	const name = req.params.tokenName;
 
+	tlogger.info(`Calling service to delete token with name '${name}'`);
 	tokensService.deleteTokenByName(name).then(result => {
+		tlogger.info('Sending response with token deleted');
 		res.status(HTTP_STATUS.OK.code).json(new Response(HTTP_STATUS.OK.code, HTTP_STATUS.OK.msg, result, 'Token deleted.'));
 	}).catch(err => {
+		tlogger.info(`Failed to delete token with name '${name}'`);
 		next(err);
 	});
 };

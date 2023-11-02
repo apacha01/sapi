f428ecb003930fbe7ee6ed53acf3ec16be5778a6
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-11-02 11:18:10 -0300

    Added logging to login controller.

--- src/controllers/login-controller.js
+++ src/controllers/login-controller.js
@@ -1,14 +1,20 @@
 import loginService from '../services/login-service.js';
 import { generateToken } from '../lib/utils/auth.js';
 import HTTP_STATUS from '../lib/constants/http.js';
+import logger from '../lib/utils/logger.js';
+
+const llogger = logger.child({ model: 'Login', layer: 'Controller' });
 
 const checkCredentials = (req, res, next) => {
 	const { username, password } = req.body;
 
+	llogger.info('Calling service to verify if user credentials are correct');
 	loginService.checkCredentials(username, password).then(result => {
+		llogger.debug('Result: ', result);
 		if (result)
 			res.status(HTTP_STATUS.OK.code).json({ token: generateToken(result.username, result.role) });
 	}).catch(err => {
+		llogger.error(`Failed to verify user '${username}'`);
 		next(err);
 	});
 };

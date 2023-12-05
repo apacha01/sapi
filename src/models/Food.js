4f6968e6404d182a7af2b9ed00e795a2c6430265
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-12-05 14:09:29 -0300

    Add price property to foods.

--- src/models/Food.js
+++ src/models/Food.js
@@ -1,13 +1,14 @@
 import { isValidModel, validateFood, betterFoodErrors } from '../lib/utils/schemas-validation.js';
 
 class Food {
-	constructor({ name, shop_tier, sources, effect, perk, sprites }) {
+	constructor({ name, shop_tier, sources, effect, perk, sprites, price }) {
 		this.name = name;
 		this.shop_tier = shop_tier;
 		this.sources = sources;
 		this.effect = effect;
 		this.perk = perk;
 		this.sprites = sprites;
+		this.price = price;
 	}
 
 	isValid() {

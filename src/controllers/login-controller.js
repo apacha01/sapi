6e6b4225dcecb1366fe0489a57b312ef2fd9c3b2
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-12-11 20:19:07 -0300

    Fix: response object on login not being standard.

--- src/controllers/login-controller.js
+++ src/controllers/login-controller.js
@@ -2,6 +2,7 @@ import loginService from '../services/login-service.js';
 import { generateToken } from '../lib/utils/auth.js';
 import HTTP_STATUS from '../lib/constants/http.js';
 import logger from '../lib/utils/logger.js';
+import Response from '../lib/response/Response.js';
 
 const llogger = logger.child({ model: 'Login', layer: 'Controller' });
 
@@ -10,7 +11,7 @@ const checkCredentials = (req, res, next) => {
 
 	llogger.info('Calling service to verify if user credentials are correct');
 	loginService.checkCredentials(name, password).then(result => {
-		res.status(HTTP_STATUS.OK.code).json({ token: generateToken(result.name, result.role) });
+		res.status(HTTP_STATUS.OK.code).json(new Response(HTTP_STATUS.OK.code, HTTP_STATUS.OK.msg, { token: generateToken(result.name, result.role) }));
 	}).catch(err => {
 		llogger.debug({ error: err, stack: err.stack }, `Failed to verify user '${name}'`);
 		next(err);
